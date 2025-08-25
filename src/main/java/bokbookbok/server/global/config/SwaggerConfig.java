e561089f52b816fae8d81539d2e6a33eecebdc41
Chae-Yu <chaeyu2004@naver.com>
2025-08-25 20:47:24 +0900

    setting: add swagger config

---
+++ src/main/java/bokbookbok/server/global/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package bokbookbok.server.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API 명세서",
+                description = "책책책 책을 읽읍시다의 API 명세서",
+                version = "v2"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        Server prodServer = new Server();
+        prodServer.setUrl("http://3.38.65.160/");
+        prodServer.setDescription("AWS EC2 서버");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local server for testing");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .servers(List.of(localServer, prodServer));
+    }
+}
+
