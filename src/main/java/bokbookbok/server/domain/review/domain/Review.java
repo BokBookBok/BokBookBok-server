4bb51ba324cda1c4655f9726ba85d83b8fe7cdfd
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 21:34:58 +0900

    feat: add review domain

---
+++ src/main/java/bokbookbok/server/domain/review/domain/Review.java
@@ -0,0 +1,35 @@
+package bokbookbok.server.domain.review.domain;
+
+import bokbookbok.server.domain.book.domain.Book;
+import bokbookbok.server.domain.user.domain.User;
+import bokbookbok.server.global.config.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "reviews")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private String content;
+
+    private int likeCount;
+
+}
