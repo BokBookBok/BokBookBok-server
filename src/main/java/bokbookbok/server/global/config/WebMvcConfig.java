5d0c3840d33fc29f99977d270b5769cad983b317
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 16:50:42 +0900

    chore: add webmvc config

---
+++ src/main/java/bokbookbok/server/global/config/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package bokbookbok.server.global.config;
+
+import bokbookbok.server.domain.auth.interceptor.AdminCheckInterceptor;
+import bokbookbok.server.global.config.resolver.CurrentUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+    private final CurrentUserArgumentResolver currentUserArgumentResolver;
+    private final AdminCheckInterceptor adminCheckInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminCheckInterceptor)
+                .addPathPatterns("/api/admin/**");
+    }
+}
+
