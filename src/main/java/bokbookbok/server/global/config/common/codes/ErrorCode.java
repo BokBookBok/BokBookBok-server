c065fc4f1d8948ba0a4544072be0c8af2755c93c
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 18:05:42 +0900

    chore: add error code & success code

--- src/main/java/bokbookbok/server/global/config/common/codes/ErrorCode.java
+++ src/main/java/bokbookbok/server/global/config/common/codes/ErrorCode.java
@@ -19,6 +19,11 @@ public enum ErrorCode {
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, 4009, "현재 비밀번호 값이 일치하지 않습니다."),
     INVALID_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, 4010,"이미지 파일만 업로드 가능합니다."),
     INVALID_EMAIL_TYPE(HttpStatus.BAD_REQUEST, 4025, "이메일 형식이 올바르지 않습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 4020, "유효하지 않은 토큰입니다."),
+    INVALID_DIRECTORY_ROUTE(HttpStatus.NOT_FOUND, 4022, "잘못된 디렉토리 경로입니다."),
+    FILE_NAME_NOT_FOUND(HttpStatus.NOT_FOUND, 4021, "파일명이 없습니다."),
+
+
 
     // 5000~ : server error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5000, "서버 오류가 발생했습니다.");
--- src/main/java/bokbookbok/server/global/config/common/codes/SuccessCode.java
+++ src/main/java/bokbookbok/server/global/config/common/codes/SuccessCode.java
@@ -12,6 +12,7 @@ public enum SuccessCode {
     USER_REGISTERED(HttpStatus.CREATED, 2001, "회원가입이 완료되었습니다."),
     LOGIN_SUCCESS(HttpStatus.OK, 2002, "로그인에 성공하였습니다."),
     USER_EMAIL_FOUND(HttpStatus.OK, 2003, "아이디 조회에 성공하였습니다."),
+    REGISTER_THIS_WEEK_BOOK(HttpStatus.OK, 2004, "금주 책 정보가 업로드 되었습니다."),
     UPDATE_USER_INFO_SUCCESS(HttpStatus.OK, 2006, "회원 정보를 수정하였습니다."),
     N_TOKEN_SUCCESS(HttpStatus.OK, 2025, "관리자 토큰이 발급되었습니다."),
     CHECK_EMAIL_DUPLICATED(HttpStatus.OK, 2014, "이메일 중복 확인이 완료되었습니다."),
