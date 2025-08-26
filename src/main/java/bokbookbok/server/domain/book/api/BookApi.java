b4a3aecde2fc673cb86751bdf16747f524915f59
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 20:11:23 +0900

    feat: add change user's reading status & get status api

---
+++ src/main/java/bokbookbok/server/domain/book/api/BookApi.java
@@ -0,0 +1,42 @@
+package bokbookbok.server.domain.book.api;
+
+import bokbookbok.server.domain.book.application.BookService;
+import bokbookbok.server.domain.book.dto.request.UpdateBookStatusRequest;
+import bokbookbok.server.domain.book.dto.response.BookStatusResponse;
+import bokbookbok.server.domain.user.domain.User;
+import bokbookbok.server.global.config.common.codes.SuccessCode;
+import bokbookbok.server.global.config.common.response.BaseResponse;
+import bokbookbok.server.global.config.resolver.CurrentUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/books")
+@AllArgsConstructor
+public class BookApi {
+
+    private final BookService bookService;
+
+    @GetMapping("/{bookId}/status")
+    @Operation(description = "유저의 독서 상태 조회")
+    public BaseResponse<BookStatusResponse> getBookStatus(
+            @Parameter(hidden = true) @CurrentUser User user,
+            @PathVariable Long bookId) {
+        BookStatusResponse bookStatusResponse = bookService.getBookStatus(user, bookId);
+        return BaseResponse.of(SuccessCode.CHECK_READING_STATUS_SUCCESS, bookStatusResponse);
+    }
+
+    @PatchMapping("/{bookId}/status")
+    @Operation(description = "유저의 독서 상태 변경")
+    public BaseResponse<BookStatusResponse> updateBookStatus(
+            @Parameter(hidden = true) @CurrentUser User user,
+            @PathVariable Long bookId,
+            UpdateBookStatusRequest updateBookStatusRequest) {
+
+        BookStatusResponse updateBookStatusResponse = bookService.updateBookStatus
+                (bookId, user, updateBookStatusRequest);
+        return BaseResponse.of(SuccessCode.CHANGE_READING_STATUS_SUCCESS, updateBookStatusResponse);
+    }
+}
---
+++ src/main/java/bokbookbok/server/domain/book/application/BookService.java
@@ -0,0 +1,64 @@
+package bokbookbok.server.domain.book.application;
+
+import bokbookbok.server.domain.book.domain.Book;
+import bokbookbok.server.domain.book.domain.enums.Status;
+import bokbookbok.server.domain.book.dto.request.UpdateBookStatusRequest;
+import bokbookbok.server.domain.book.dto.response.BookStatusResponse;
+import bokbookbok.server.domain.book.repository.BookRepository;
+import bokbookbok.server.domain.record.dao.UserBookRecordRepository;
+import bokbookbok.server.domain.record.domain.UserBookRecord;
+import bokbookbok.server.domain.user.domain.User;
+import bokbookbok.server.global.config.common.codes.ErrorCode;
+import bokbookbok.server.global.config.exception.BusinessExceptionHandler;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+    private final UserBookRecordRepository recordRepository;
+
+    @Transactional
+    public BookStatusResponse updateBookStatus(Long bookId, User user, UpdateBookStatusRequest updateBookStatusRequest) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BusinessExceptionHandler(ErrorCode.NOT_EXISTS_BOOK));
+
+        Status newStatus = updateBookStatusRequest.getStatus();
+
+        UserBookRecord record = recordRepository.findByUserAndBook(user, book).orElse(null);
+        if (record == null) {
+            record = UserBookRecord.builder()
+                    .user(user)
+                    .book(book)
+                    .status(newStatus)
+                    .build();
+        } else {
+            record.updateStatus(newStatus);
+        }
+
+        UserBookRecord savedRecord = recordRepository.save(record);
+
+        return BookStatusResponse.from(savedRecord);
+    }
+
+    public BookStatusResponse getBookStatus(User user, Long bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BusinessExceptionHandler(ErrorCode.NOT_EXISTS_BOOK));
+
+        UserBookRecord record = recordRepository.findByUserAndBook(user, book).orElse(null);
+
+        Status status;
+        if (record != null) {
+            status = record.getStatus();
+        } else {
+            status = Status.NOT_STARTED;
+        }
+
+        return BookStatusResponse.builder()
+                .bookId(book.getId())
+                .status(status)
+                .build();
+    }
+}
