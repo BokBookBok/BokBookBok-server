b7f5c795cdc9da3f5d03ac044362635a1ef1bd5f
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 14:11:30 +0900

    feat: add user domain & repository

---
+++ src/main/java/bokbookbok/server/domain/user/domain/User.java
@@ -0,0 +1,27 @@
+package bokbookbok.server.domain.user.domain;
+
+import bokbookbok.server.global.config.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+}
