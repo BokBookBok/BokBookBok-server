51ef4e6e778c506de636935dbef29f6c88cf9d53
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 14:27:31 +0900

    fix: add Role ENUM

--- src/main/java/bokbookbok/server/domain/user/domain/User.java
+++ src/main/java/bokbookbok/server/domain/user/domain/User.java
@@ -1,5 +1,6 @@
 package bokbookbok.server.domain.user.domain;
 
+import bokbookbok.server.domain.user.domain.enums.Role;
 import bokbookbok.server.global.config.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -24,4 +25,7 @@ public class User extends BaseEntity {
 
     @Column(nullable = false, length = 255)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
---
+++ src/main/java/bokbookbok/server/domain/user/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package bokbookbok.server.domain.user.domain.enums;
+
+public enum Role {
+    USER, ADMIN
+}
