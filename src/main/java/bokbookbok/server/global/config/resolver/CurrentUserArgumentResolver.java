8712d149ca3526199205d74c10521771eece3dd7
Chae-Yu <chaeyu2004@naver.com>
2025-08-26 16:50:24 +0900

    feat: add currentUser annotation

---
+++ src/main/java/bokbookbok/server/global/config/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,38 @@
+package bokbookbok.server.global.config.resolver;
+
+import bokbookbok.server.domain.user.domain.User;
+import bokbookbok.server.global.config.common.codes.ErrorCode;
+import bokbookbok.server.global.config.exception.BusinessExceptionHandler;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null ||!(authentication.getPrincipal() instanceof User)){
+            throw new BusinessExceptionHandler(ErrorCode.INVALID_TOKEN);
+        }
+
+        return authentication.getPrincipal();
+    }
+}
