dd4e03c2d836b26ccf18552108c9bd77d8cef810
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 10:14:40 +0900

    chore: add swagger description

--- src/main/java/bokbookbok/server/domain/home/api/HomeApi.java
+++ src/main/java/bokbookbok/server/domain/home/api/HomeApi.java
@@ -6,6 +6,7 @@ import bokbookbok.server.domain.user.domain.User;
 import bokbookbok.server.global.config.common.codes.SuccessCode;
 import bokbookbok.server.global.config.common.response.BaseResponse;
 import bokbookbok.server.global.config.resolver.CurrentUser;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ public class HomeApi {
 
     private final HomeService homeService;
     @GetMapping
+    @Operation(summary = "메인 화면", description = "유저 독서 상태별로 다름")
     public BaseResponse<HomeResponse> getHome(@Parameter(hidden = true) @CurrentUser User user) {
         HomeResponse homeResponse = homeService.getHome(user);
         return BaseResponse.of(SuccessCode.GET_HOME_INFO, homeResponse);
