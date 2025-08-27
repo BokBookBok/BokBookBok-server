188fb1794bc2162786a8dec96142e643bd5963d3
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 10:01:48 +0900

    feat: add find current book

--- src/main/java/bokbookbok/server/domain/book/repository/BookRepository.java
+++ src/main/java/bokbookbok/server/domain/book/repository/BookRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Modifying
     @Query("UPDATE Book b SET b.isCurrent = false WHERE b.isCurrent = true")
     void resetAllCurrentFlags();
+
+    @Query("SELECT b FROM Book b WHERE b.isCurrent = true")
+    Optional<Book> findCurrentBook();
+
 }
