00f7f03769e8c3e7b79ab0ae030197f927655633
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 09:59:11 +0900

    feat: add book info response dto

---
+++ src/main/java/bokbookbok/server/domain/book/dto/response/BookInfoResponse.java
@@ -0,0 +1,23 @@
+package bokbookbok.server.domain.book.dto.response;
+
+import bokbookbok.server.domain.book.domain.Book;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BookInfoResponse {
+    private Long id;
+    private String title;
+    private String author;
+    private String imageUrl;
+
+    public static BookInfoResponse from(Book book) {
+        return BookInfoResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .author(book.getAuthor())
+                .imageUrl(book.getImageUrl())
+                .build();
+    }
+}
