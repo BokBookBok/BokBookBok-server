1f44af3365ed52e0e14afa64e66906ade0dcbe7e
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 10:01:57 +0900

    feat: add find top review

--- src/main/java/bokbookbok/server/domain/review/dao/ReviewRepository.java
+++ src/main/java/bokbookbok/server/domain/review/dao/ReviewRepository.java
@@ -6,9 +6,16 @@ import bokbookbok.server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     boolean existsByUserAndBook(User user, Book book);
+
     List<Review> findAllByBook(Book book);
 
+    Optional<Review> findTopByBookOrderByLikeCountDescCreatedAtAsc(Book book);
+
+    Optional<Review> findTopByUserAndBookOrderByCreatedAtDesc(User user, Book book);
+
 }
+
