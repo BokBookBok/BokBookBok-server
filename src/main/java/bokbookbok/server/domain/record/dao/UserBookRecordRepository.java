463f3122ab6a7c3a2df0a7bb42881784be681a72
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 10:01:38 +0900

    feat: add calculate average reading days

--- src/main/java/bokbookbok/server/domain/record/dao/UserBookRecordRepository.java
+++ src/main/java/bokbookbok/server/domain/record/dao/UserBookRecordRepository.java
@@ -5,10 +5,13 @@ import bokbookbok.server.domain.record.domain.UserBookRecord;
 import bokbookbok.server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserBookRecordRepository extends JpaRepository<UserBookRecord, Long> {
     Optional<UserBookRecord> findByUserAndBook(User user, Book book);
 
     Optional<Object> findByBookAndUser(Book book, User user);
+
+    List<UserBookRecord> findAllByBook(Book book);
 }
