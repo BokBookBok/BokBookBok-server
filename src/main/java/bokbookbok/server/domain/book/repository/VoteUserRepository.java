86506b4115f8772167c86599109fe78c2f89dda7
Chae-Yu <chaeyu2004@naver.com>
2025-08-27 14:31:38 +0900

    feat: add repository method

--- src/main/java/bokbookbok/server/domain/book/repository/VoteUserRepository.java
+++ src/main/java/bokbookbok/server/domain/book/repository/VoteUserRepository.java
@@ -5,6 +5,9 @@ import bokbookbok.server.domain.book.domain.VoteUser;
 import bokbookbok.server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VoteUserRepository extends JpaRepository<VoteUser, Long> {
     boolean existsByVoteAndUser(Vote vote, User user);
+    Optional<VoteUser> findByVoteAndUser(Vote vote, User user);
 }
